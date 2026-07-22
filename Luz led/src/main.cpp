8ca178d71eae0d7912d7aa5c972bbf11e7dee1e1
julio-graterol-ing <julioabgraterol@gmail.com>
2026-07-22 22:56:12 +0200

    feat: complete non-blocking DHT11 environmental climate sensing telemetry

--- Luz led/src/main.cpp
+++ Luz led/src/main.cpp
@@ -1,58 +1,55 @@
 #include <Arduino.h> //Required library for Arduino framework in PlatformIO
-#include <Servo.h> // Native library for hardware servo control
+#include <DHT.h> // offical Adafruit for enviromental climate sensitive
 
-// Hardware pin assigment
-const int PIN_POTENTIOMETER = A0; // Analog input for position control
-const int PIN_SERVO = 9;    // PWM output to drive the servo motor
+//Hardware Pin and Sensor Type Configuration
+const int PIN_DHT = 2; // Digital pin connected to the DHT11 data line
+#define DHTTYPE DHT11 //Define the specific sensor hardware type
 
-//Instantiate the hardware servo driver objects
-Servo myServo;
+// Instatiate the enviromental sensor driver object
+DHT dht(PIN_DHT, DHTTYPE);
 
+//System State Variables
+float temperatureC = 0.0; //Store the calculated temperature in celsius
+float humidityPct = 0.0; //Store the calculated relative humidity percentage
 
-// System State Variables
-int rawAnalogValue = 0; //Stores raw data from the knob (0 to 1023)
-int targetAngle = 0; //Store calculated mechanical angle (0 to 1023)
+//Non-Blocking Telemetry Timer Variables
+unsigned long previousClimateMillis = 0;
+const unsigned long CLIMATE_INTERVAL = 2000; //DHT11 sensor requires at least 2 seconds between reads
 
-// Non blocking Telemetry Timer Variables
-unsigned long previousMillis = 0;         // Stores the last time telemetry was sent
-const unsigned long SERVO_TELEMETRY_INTERVAL = 200;  // Update print screen every 200ms
-
-// SETUP: Runs once when the microcontroller starts or resets
-void setup()
-{
-
-  // Initialize communication for physical diagnostics telemetry
+void setup() {
+  // Initialize communication for physical diagnostic logs
   Serial.begin(9600);
 
-  //Attach the servo object to its physical digital control pin
-  myServo.attach(PIN_SERVO);
+  //Initialize the physical internal registers of the DHT sensor
+  dht.begin();
+
 }
 
-void loop()
-{
+void loop() {
 
-  //1. Read: Capture the real time position of the potentiometer knob
-  rawAnalogValue = analogRead(PIN_POTENTIOMETER);
-
-  //2. Process: Map 10 bit input scale (0-1023) to mechanical angle limits (0-180)
-  targetAngle = map(rawAnalogValue, 0, 1023, 0, 180);
-
-  //3. Execute: Transmit the target angle comand to the motor driver hardware
-  myServo.write(targetAngle);
-
-  //4. Telemetry: Non blocking time checker using the system clock
+  //Non blocking execution time checker using the system clock
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= SERVO_TELEMETRY_INTERVAL) {
-  previousMillis = currentMillis;
+  if (currentMillis - previousClimateMillis >= CLIMATE_INTERVAL) {
+    previousClimateMillis = currentMillis;
 
-  // Send mechanical diagnostics data to the computer monitor screen
-  Serial.print("Potentiometer Input: ");
-  Serial.print(rawAnalogValue);
-  Serial.print(" | Actuator Angle Command: ");
-  Serial.print(targetAngle);
-  Serial.println(" deg");
+    //1. Read: Capture raw digital streams and convert to floating point metrics
+    humidityPct = dht.readHumidity();
+    temperatureC = dht.readTemperature();
 
-}
+    //2. Error Checking: Validate if the data received from the hardware is valid
+     if (isnan(humidityPct) || isnan (temperatureC)) {
+      Serial.println("CRITICAL ERROR:Failed to read from DHT sensor Hardware");
+      return; // Exit this cycle immediately to prevent bad telemetry output
+     }
 
-}
+     //3. Telemetry Output: Transmit Performance data to the monitor screen
+     Serial.print("CLIMATE DATA | Humidity: ");
+     Serial.print(humidityPct);
+     Serial.print("% | Temperature: ");
+     Serial.print(temperatureC);
+     Serial.println("*C");
+     }
+  
+  }
+
