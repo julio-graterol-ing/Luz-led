5d5d3fc090ba329941f56bc93b6db9dee871ad30
julio-graterol-ing <julioabgraterol@gmail.com>
2026-07-22 22:02:40 +0200

    feat: complete automated servo-control system with successful analog hardware mapping

--- Luz led/src/main.cpp
+++ Luz led/src/main.cpp
@@ -1,23 +1,21 @@
 #include <Arduino.h> //Required library for Arduino framework in PlatformIO
-#include <NewPing.h> // Professional non Blocking ultrasonic library
+#include <Servo.h> // Native library for hardware servo control
 
 // Hardware pin assigment
-const int PIN_TRIGGER = 12; // Outbound ultrasonic pulse pin
-const int PIN_ECHO = 11;    // Inbound ultrasonic return pin
-const int PIN_LED = 13;     // Proximity alert indicator LED
+const int PIN_POTENTIOMETER = A0; // Analog input for position control
+const int PIN_SERVO = 9;    // PWM output to drive the servo motor
 
-// Maximum distance we want to ping for (in centimeters). Macimum sensor limit is 400-500cm
-const int MAX_DISTANCE = 200;
+//Instantiate the hardware servo driver objects
+Servo myServo;
 
-// NewPing instance setup using our hardware configuration
-NewPing sonar(PIN_TRIGGER, PIN_ECHO, MAX_DISTANCE);
 
 // System State Variables
-unsigned int distanceCm = 0; // Stores the real time distance in centimeters
+int rawAnalogValue = 0; //Stores raw data from the knob (0 to 1023)
+int targetAngle = 0; //Store calculated mechanical angle (0 to 1023)
 
 // Non blocking Telemetry Timer Variables
 unsigned long previousMillis = 0;         // Stores the last time telemetry was sent
-const unsigned long RADAR_INTERVAL = 150; // Sample and print distance
+const unsigned long SERVO_TELEMETRY_INTERVAL = 200;  // Update print screen every 200ms
 
 // SETUP: Runs once when the microcontroller starts or resets
 void setup()
@@ -26,45 +24,35 @@ void setup()
   // Initialize communication for physical diagnostics telemetry
   Serial.begin(9600);
 
-  // Pin peripherals configuration
-  pinMode(PIN_LED, OUTPUT); // PWM pin set as output
+  //Attach the servo object to its physical digital control pin
+  myServo.attach(PIN_SERVO);
 }
 
 void loop()
 {
 
-  // Non blocking execution time checker
+  //1. Read: Capture the real time position of the potentiometer knob
+  rawAnalogValue = analogRead(PIN_POTENTIOMETER);
+
+  //2. Process: Map 10 bit input scale (0-1023) to mechanical angle limits (0-180)
+  targetAngle = map(rawAnalogValue, 0, 1023, 0, 180);
+
+  //3. Execute: Transmit the target angle comand to the motor driver hardware
+  myServo.write(targetAngle);
+
+  //4. Telemetry: Non blocking time checker using the system clock
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= RADAR_INTERVAL)
-  {
-    previousMillis = currentMillis;
+  if (currentMillis - previousMillis >= SERVO_TELEMETRY_INTERVAL) {
+  previousMillis = currentMillis;
 
-    // 1. Read & Process: Send a ping, get return time and convert directly to centimeters
-    distanceCm = sonar.ping_cm();
+  // Send mechanical diagnostics data to the computer monitor screen
+  Serial.print("Potentiometer Input: ");
+  Serial.print(rawAnalogValue);
+  Serial.print(" | Actuator Angle Command: ");
+  Serial.print(targetAngle);
+  Serial.println(" deg");
 
-    // 2. Conditional Logic: Evaluate proximity thresholds
-    // If an object is detected closer than 15 centimeters (ignoring 0 wich means out og range)
-    if (distanceCm < 15 && distanceCm > 0)
-    {
-      digitalWrite(PIN_LED, HIGH); // Activate visual ganger alert
-      Serial.print("WARNING! Object detected at: ");
-    }
-    else
-    {
-      digitalWrite(PIN_LED, LOW); // Clear visual alert
-      Serial.print("Safe zone. Distance: ");
-    }
+}
 
-    // 3.Telemetry Output: Print the final engineering values
-    if (distanceCm == 0)
-    {
-      Serial.println("Out of range or no obstacle");
-    }
-    else
-    {
-      Serial.print(distanceCm);
-      Serial.println(" cm");
-    }
-  }
 }
